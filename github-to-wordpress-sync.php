02e02364892c072cb8799bb56da7d1ea65f66a7f
sinanisler <sinan@sinanisler.com>
2025-12-19 15:11:29 +0300

    release

--- github-to-wordpress-sync.php
+++ github-to-wordpress-sync.php
@@ -1,5 +1,5 @@
 <?php
-/**    
+/**     
  * Plugin Name: Github to WordPress Sync
  * Plugin URI: https://github.com/sinanisler/github-to-wordpress-sync
  * Description: GitHub to WordPress Sync: Streamline theme & plugin updates directly from GitHub. Easy, secure, and developer-friendly.
